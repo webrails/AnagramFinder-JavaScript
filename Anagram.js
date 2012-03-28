46680d0ff81479f3beca5039800030a7221516bb
Marc Bungart <dev@webrails.de>
2012-03-28 22:36:56 +0200

    Initial commit

---
+++ Anagram.js
@@ -0,0 +1,12 @@
+function Anagram() {
+    "use strict";
+    this.find = function (params) {
+        return _.filter(_.groupBy(Array.prototype.slice.call(arguments), function (word) {
+            return _.sortBy(word.split(''), function (character) {
+                    return character;
+                }).join('');
+        }), function (group) {
+            return group.length > 1;
+        });
+    };
+}
---
+++ Anagram.spec.js
@@ -0,0 +1,26 @@
+(function () {
+    "use strict";
+    describe("Anagram", function () {
+        var kataBankOCR;
+        beforeEach(function () {
+            this.anagramFinder = new Anagram();
+        });
+        it("should be instantiable", function () {
+            expect(this.anagramFinder).toBeTruthy();
+        });
+        describe("find", function () {
+            it("should be a function", function () {
+                expect(_.isFunction(this.anagramFinder.find)).toBeTruthy();
+            });
+            it("should return an empty Array", function () {
+                expect(this.anagramFinder.find()).toEqual([]);
+            });
+            it("should return an empty Array", function () {
+                expect(this.anagramFinder.find("meat", "mat", "team", "mate", "eat")).toEqual([["meat", "team", "mate"]]);
+            });
+            it("should return an empty Array", function () {
+                expect(this.anagramFinder.find("veer", "lake", "item", "kale", "mite", "ever")).toEqual([["veer", "ever"], ["lake", "kale"], ["item", "mite"]]);
+            });
+        });
+    });
+})();
