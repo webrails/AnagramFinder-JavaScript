924cbc8fec1ce7927f5c0fdf55da06e8f6ed098a
Marc Bungart <dev@webrails.de>
2012-03-28 22:48:32 +0200

    Unnecessary parameter in find removed

--- Anagram.js
+++ Anagram.js
@@ -1,6 +1,6 @@
 function Anagram() {
     "use strict";
-    this.find = function (params) {
+    this.find = function () {
         return _.filter(_.groupBy(Array.prototype.slice.call(arguments), function (word) {
             return _.sortBy(word.split(''), function (character) {
                     return character;
